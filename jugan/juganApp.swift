ff34496a56778b19202245adb1bd9dea582b6603
jklee <jkleeyt60@gmail.com>
2026-02-26 22:50:10 +0900

    Initial Commit

---
+++ jugan/juganApp.swift
@@ -0,0 +1,17 @@
+//
+//  juganApp.swift
+//  jugan
+//
+//  Created by Lee MacMini on 2/26/26.
+//
+
+import SwiftUI
+
+@main
+struct juganApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
